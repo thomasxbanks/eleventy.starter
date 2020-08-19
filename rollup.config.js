bf0db92a0d12386d0c6419e2eceef1ad5b417f18
Thomas Banks <thomasxbanks@gmail.com>
2020-08-19 13:34:08 +0100

    Adds rollup plugins to handle node_module imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,20 @@
 // rollup.config.js
+
+import common from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from 'rollup-plugin-json';
+import globals from 'rollup-plugin-node-globals';
+
 export default {
   input: 'src/js/main.js',
   output: {
     file: 'dist/js/index.js',
     format: 'cjs',
   },
+  plugins: [
+    common(),
+    globals(),
+    resolve(),
+    json(),
+  ],
 };
