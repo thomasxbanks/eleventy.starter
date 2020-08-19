cef46012e0306fec3365c104cd5001eab3538c39
Thomas Banks <thomasxbanks@gmail.com>
2020-08-19 13:34:47 +0100

    Adds PWA functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@ import dev from './dev';
 
 const init = async () => {
   dev();
+  if ('serviceWorker' in navigator) {
+    console.log('>>> serviceWorker in navigator');
+    navigator.serviceWorker.register('../service-worker.js').then(() => {
+      console.log('Service Worker Registered');
+    });
 };
 
 window.onload = init;
